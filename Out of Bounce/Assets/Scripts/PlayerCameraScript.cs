ca89be28986bcd139f2a7ccb7cfc7818d46bb94d
Tim Wahrburg <t.wahrburg@gmail.com>
2022-07-06 18:52:04 +0200

    initial commit with updated .gitignore. Hello, Git!

---
+++ Out of Bounce/Assets/Scripts/PlayerCameraScript.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerCameraScript : MonoBehaviour
+{
+    [Header("Dependencies")]
+    [SerializeField]
+    private PlayerController _playerController;
+
+
+    [Header("Configuration")]
+    [SerializeField, Range(0f, 1f)]
+    private float _mouseSensitivity;
+
+    private PlayerInput _playerInput;
+
+    private InputAction _lookAction;
+
+    private Vector2 currentLook = Vector3.forward;
+
+    private void Awake()
+    {
+        _playerInput = GetComponent<PlayerInput>();
+        _lookAction = _playerInput.actions["Look"];
+    }
+
+    private void Update()
+    {
+        HandleLookDirection();
+    }
+
+    private void HandleLookDirection()
+    {
+        // horizontal bewegt sich Spieler, Vertikal nur die Kamera
+
+        Vector2 lookDelta = _lookAction.ReadValue<Vector2>();
+
+        lookDelta.x *= _mouseSensitivity;
+        lookDelta.y *= _mouseSensitivity;
+
+        currentLook.x += lookDelta.x;
+        currentLook.y = Mathf.Clamp(currentLook.y += lookDelta.y, -90, 90);
+
+        transform.localRotation = Quaternion.AngleAxis(-currentLook.y, Vector3.right);
+        _playerController.transform.localRotation = Quaternion.Euler(0, currentLook.x, 0);
+
+    }
+}
---
+++ Out of Bounce/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,265 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [Header("Dependencies")]
+    [SerializeField]
+    PhysicMaterial _playerPhysicsMaterial = default;
+
+    [Header("Configurations")]
+    [SerializeField, Range(0f, 100f)]
+    float _maxSpeed = 5f;
+
+    [SerializeField, Range(0f, 100f)]
+    float _maxAcceleration = 5f, _maxAirAcceleration = 1f;
+
+    [SerializeField, Range(0f, 100f)]
+    float _manualJumpHeight = 5f;
+
+    [SerializeField, Range(0f, 100f)]
+    float _automaticJumpHeight;
+
+    [SerializeField, Range(0f, 5f)]
+    int _maxAirJumps = 0;
+
+    [SerializeField, Range(0f, 90f)]
+    float _maxGroundAngle = 60f;
+
+    
+
+    
+
+    // Properties
+    bool OnGround => _groundContactCount > 0;
+
+    // Inputs
+    private PlayerInput _playerInput;
+    private InputAction _jumpAction;
+    private InputAction _movementAction;
+
+    private Vector2 _playerInputMovementVector;
+
+    // Components
+    private Rigidbody _rb;
+
+    // Physics Forces
+    private Vector3 _gravity = Physics.gravity;
+    
+    // Velocity
+    private Vector3 _velocity;
+    private Vector3 _desiredForwardVelocity, _desiredRightVelocity = Vector3.zero;
+
+    // Flags
+    private bool _desiresJump = false;
+    private bool _hasBouncedThisFrame = false;
+
+    // Collision
+    int stepsSinceLastGrounded, stepsSinceLastJumped;
+    int _groundContactCount;
+    float _minGroundDotProduct;
+    Vector3 _contactNormal;
+    Vector3 _upAxis = Vector3.up;
+
+    // Jump
+    int _jumpPhase;
+
+    
+
+    private void Awake()
+    {
+        // Set Rigidbody
+        _rb = GetComponent<Rigidbody>();
+        _rb.useGravity = false;
+
+        // Set PlayerInputs
+        _playerInput = GetComponent<PlayerInput>();
+        _jumpAction = _playerInput.actions["Jump"];
+        _movementAction = _playerInput.actions["Move"];
+
+
+    }
+    private void OnValidate()
+    {
+        _minGroundDotProduct = Mathf.Cos(_maxGroundAngle * Mathf.Deg2Rad);
+    }
+
+    private void Update()
+    {
+        HandlePlayerMovement();
+
+        if (_jumpAction.WasPressedThisFrame())
+        {
+            _desiresJump = true;
+        }
+        
+
+    }
+
+    private void HandlePlayerMovement()
+    {
+        _playerInputMovementVector = _movementAction.ReadValue<Vector2>();
+        _playerInputMovementVector = Vector2.ClampMagnitude(_playerInputMovementVector, 1f);
+
+        _desiredForwardVelocity = _playerInputMovementVector.y * transform.forward * _maxSpeed;
+        _desiredRightVelocity = _playerInputMovementVector.x * transform.right * _maxSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        // Get Velocity
+        _velocity = _rb.velocity;
+
+        UpdateState();
+        AdjustVelocity();
+
+        if (_desiresJump)
+        {
+            _desiresJump = false;
+            Jump(_manualJumpHeight);
+        }
+        else if (OnGround)
+        {
+            //Jump(_automaticJumpHeight);
+        }
+
+        // Add Gravity
+        _velocity += _gravity * Time.fixedDeltaTime;
+
+        // Set Velocity
+        _rb.velocity = _velocity;
+
+        // ClearState
+        ClearState();
+        
+    }
+
+    private void Jump(float jumpHeight)
+    {
+        Vector3 jumpDirection;
+
+        if (OnGround)
+        {
+            jumpDirection = _contactNormal;
+        }
+        else if (_maxAirJumps > 0 && _jumpPhase <= _maxAirJumps)
+        {
+            if (_jumpPhase == 0)
+            {
+                _jumpPhase = 1;
+            }
+            jumpDirection = _contactNormal;
+        }
+        else
+        {
+            return;
+        }
+
+
+        stepsSinceLastJumped = 0;
+        _jumpPhase += 1;
+        float jumpSpeed = Mathf.Sqrt(2f * _gravity.magnitude * jumpHeight);
+
+        float alignedSpeed = Vector3.Dot(_velocity, jumpDirection);
+
+        if (alignedSpeed > 0f)
+        {
+            jumpSpeed = Mathf.Max(jumpSpeed - alignedSpeed, 0f);
+        }
+
+        
+        _velocity += jumpDirection * jumpSpeed;
+    }
+
+    private void UpdateState()
+    {
+        stepsSinceLastGrounded += 1;
+        stepsSinceLastJumped += 1;
+
+        if (OnGround)
+        {
+            stepsSinceLastGrounded = 0;
+            if (stepsSinceLastJumped > 1)
+            {
+                _jumpPhase = 0;
+            }
+        }
+        else
+        {
+            _contactNormal = _upAxis;
+        }
+    }
+
+
+    private void ClearState()
+    {
+        _contactNormal = Vector3.zero;
+        _groundContactCount = 0;
+        _hasBouncedThisFrame = false;
+    }
+
+    private void AdjustVelocity()
+    {
+        float acceleration = OnGround ? _maxAcceleration : _maxAirAcceleration;
+
+        float maxSpeedChange = acceleration * Time.deltaTime;
+
+        float velocityDotForward = Vector3.Dot(_velocity, transform.forward);
+        float velocityDotRight = Vector3.Dot(_velocity, transform.right);
+
+        float newForward = Mathf.MoveTowards(velocityDotForward, Vector3.Dot(transform.forward, _desiredForwardVelocity), maxSpeedChange);
+        float newRight = Mathf.MoveTowards(velocityDotRight, Vector3.Dot(transform.right, _desiredRightVelocity), maxSpeedChange);
+
+        Vector3 movement = transform.forward * (newForward - velocityDotForward) + transform.right * (newRight - velocityDotRight);
+
+        _velocity += movement;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        EvaluateCollision(collision);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        EvaluateCollision(collision);
+    }
+
+    private void EvaluateCollision(Collision collision)
+    {
+        for (int i = 0; i < collision.contactCount; i++)
+        {
+            Vector3 normal = collision.GetContact(i).normal;
+            float upDot = Vector3.Dot(_upAxis, normal);
+
+            if (upDot >= _minGroundDotProduct)
+            {
+                _groundContactCount += 1;
+                _contactNormal += normal;
+            }            
+        }
+
+        if (collision.contactCount > 1)
+        {
+            _contactNormal.Normalize();
+        }
+
+        // it's time to
+        // bounce
+        Bounce();
+        
+    }
+
+    private void Bounce()
+    {
+        _hasBouncedThisFrame = true;
+    }
+
+    Vector3 ProjectDirectionOnPlane(Vector3 direction, Vector3 normal)
+    {
+        return (direction - normal * Vector3.Dot(direction, normal)).normalized;
+    }
+}
